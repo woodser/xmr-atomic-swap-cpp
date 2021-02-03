9de862705bb1f1dd04eb7a69cb54c72cd4278a60
woodser <woodser@protonmail.com>
2021-02-03 11:53:01 -0500

    building bitcoin and monero keypairs in rust

--- rust_bridge/src/atomic_swap_rust_bridge.rs
+++ rust_bridge/src/atomic_swap_rust_bridge.rs
@@ -6,7 +6,10 @@ pub extern "C" fn greetings()
   println!("Hello from Rust library file!");
   
   let rng = &mut OsRng;
+  let a = swap::bitcoin::SecretKey::new_random(rng);
   let s_a = cross_curve_dleq::Scalar::random(rng);
+  let v_a = swap::monero::PrivateViewKey::new_random(rng);
+  println!("OK?");
 }
 
 #[no_mangle]
