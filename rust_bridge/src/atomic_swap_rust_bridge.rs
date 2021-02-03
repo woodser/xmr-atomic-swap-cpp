a07f7037621e1ea25b14d94e404c0947fbba3014
woodser <woodser@protonmail.com>
2021-02-03 12:34:21 -0500

    print private key as json with int arrays

--- rust_bridge/src/atomic_swap_rust_bridge.rs
+++ rust_bridge/src/atomic_swap_rust_bridge.rs
@@ -1,15 +1,18 @@
-use rand::rngs::OsRng;
-
 #[no_mangle]
 pub extern "C" fn greetings() 
 {
   println!("Hello from Rust library file!");
   
-  let rng = &mut OsRng;
+  // generate keys
+  let rng = &mut rand::rngs::OsRng;
   let a = swap::bitcoin::SecretKey::new_random(rng);
   let s_a = cross_curve_dleq::Scalar::random(rng);
   let v_a = swap::monero::PrivateViewKey::new_random(rng);
-  println!("OK?");
+  println!("Keys generated");
+  
+  // print keys
+  let a_str = serde_json::to_string(&a).expect("failed to serialize bitcoin secret key a");
+  println!("Bitcoin private key a: {:?}", a_str);
 }
 
 #[no_mangle]
